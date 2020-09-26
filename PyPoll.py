9e6320c2ab3f66e7d8dceab1a542d93f189d7a4e
Alex Felice <golfzander@comcast.net>
2020-09-26 09:52:21 -0500

    Update PyPoll.py

--- PyPoll.py
+++ PyPoll.py
@@ -12,6 +12,14 @@ import os
 import random
 import csv
 
+# Declare variables
+total_votes = 0
+candidate_options = []
+candidate_votes = {}
+winning_candidate = ""
+winning_count = 0
+winning_percentage = 0
+
 now = dt.datetime.now()
 
 # Assign a variable for the file to load and the path
@@ -32,9 +40,40 @@ with open(file_to_load, 'r') as election_data:
     headers = next(file_reader)
     # print(headers)
 
-    # Print each row in the CSV file.
+    # Tally total votes, Add candidates to list, tally votes for each candidate
     for row in file_reader:
-        print(row)
+
+        # Tally total votes
+        total_votes += 1
+        candidate_name = row[2]
+
+        # If current name is not already in list, add candidate to list/dictionary and set their vote tally to zero
+        if candidate_name not in candidate_options:
+            candidate_options.append(candidate_name)
+            candidate_votes[candidate_name] = 0
+        candidate_votes[candidate_name] += 1
+
+    # print out results by percentage for each candidate
+    for candidate_name in candidate_votes:
+        votes = candidate_votes[candidate_name]
+        vote_percentage = float(votes) / float(total_votes)*100
+        print(f"{candidate_name}: {vote_percentage:.1f}% ({votes:,})\n")
+        # Determine winning vote count and candidate
+        # Determine if the votes is greater than the winning count.
+        if (votes > winning_count) and (vote_percentage > winning_percentage):
+            # If true then set winning_count = votes and winning_percent =
+            # vote_percentage.
+            winning_count = votes
+            winning_percentage = vote_percentage
+            # And, set the winning_candidate equal to the candidate's name.
+            winning_candidate = candidate_name
+    winning_candidate_summary = (
+        f"-----------------------\n"
+        f"Winner: {winning_candidate}\n"
+        f"Winning Vote Count: {winning_count:,}\n"
+        f"Winning Percentage: {winning_percentage:.1f}%\n"
+        f"-----------------------\n")
+    print(winning_candidate_summary)
 
 # Close the file.
 election_data.close()
