360f939d2b36f6781eefffe8f2b79b65f8ad6088
Alex Felice <golfzander@comcast.net>
2020-09-28 07:44:54 -0500

    Adding finished code and results txt file

--- PyPoll Challenge/PyPoll_Challenge.py
+++ PyPoll Challenge/PyPoll_Challenge.py
@@ -6,9 +6,9 @@ import csv
 import os
 
 # Add a variable to load a file from a path.
-file_to_load = os.path.join("../Resources/election_results.csv")
+file_to_load = 'PyPoll Challenge\\Resources\\election_results.csv'
 # Add a variable to save the file to a path.
-file_to_save = os.path.join("analysis", "election_analysis.txt")
+file_to_save = 'PyPoll Challenge\\Analysis\\election_analysis.txt'
 
 # Initialize a total vote counter.
 total_votes = 0
@@ -18,7 +18,8 @@ candidate_options = []
 candidate_votes = {}
 
 # 1: Create a county list and county votes dictionary.
-
+county_list = []
+county_votes = {}
 
 
 # Track the winning candidate, vote count and percentage
@@ -26,9 +27,11 @@ winning_candidate = ""
 winning_count = 0
 winning_percentage = 0
 
+
 # 2: Track the largest county and county voter turnout.
-
-
+highest_turnout_county = ""
+highest_turnout = 0
+highest_percentage = 0
 
 # Read the csv and convert it into a list of dictionaries
 with open(file_to_load) as election_data:
@@ -47,7 +50,7 @@ with open(file_to_load) as election_data:
         candidate_name = row[2]
 
         # 3: Extract the county name from each row.
-
+        county_name = row[1]
 
         # If the candidate does not match any existing candidate add it to
         # the candidate list
@@ -64,16 +67,15 @@ with open(file_to_load) as election_data:
 
         # 4a: Write a decision statement that checks that the
         # county does not match any existing county in the county list.
-
-
+        if county_name not in county_list:
             # 4b: Add the existing county to the list of counties.
-
+            county_list.append(county_name)
 
             # 4c: Begin tracking the county's vote count.
-
+            county_votes[county_name] = 0
 
         # 5: Add a vote to that county's vote count.
-
+        county_votes[county_name] += 1
 
 
 # Save the results to our text file.
@@ -91,26 +93,39 @@ with open(file_to_save, "w") as txt_file:
     txt_file.write(election_results)
 
     # 6a: Write a repetition statement to get the county from the county dictionary.
-
+    for county_name in county_list:
         # 6b: Retrieve the county vote count.
-
+        c_votes = county_votes.get(county_name)
         # 6c: Calculate the percent of total votes for the county.
+        c_votes_percentage = float(c_votes) / float (total_votes) * 100
 
-
-         # 6d: Print the county results to the terminal.
-
-         # 6e: Save the county votes to a text file.
-
-         # 6f: Write a decision statement to determine the winning county and get its vote count.
+        # 6d: Print the county results to the terminal.
+        county_results = (
+            f"{county_name}: {c_votes_percentage:.1f}% ({c_votes:,})\n")
+        print(county_results)
+        # 6e: Save the county votes to a text file.
+        txt_file.write(county_results)
+        # 6f: Write a decision statement to determine the winning county and get its vote count.
+        if (c_votes > highest_turnout) and (c_votes_percentage > highest_percentage):
+            highest_turnout = c_votes
+            highest_turnout_county = county_name
+            highest_percentage = c_votes_percentage
 
 
     # 7: Print the county with the largest turnout to the terminal.
-
-
+    county_turnout_summary = (
+        f"-------------------------\n"
+        f"Highest Turnout County: {highest_turnout_county}\n"
+        f"Number of Votes in {highest_turnout_county}: {highest_turnout:,}\n"
+        f"Percentage of Total Votes: {highest_percentage:.1f}%\n"
+        f"-------------------------\n")
+    print(county_turnout_summary)
     # 8: Save the county with the largest turnout to a text file.
-
+    txt_file.write(county_turnout_summary)
 
     # Save the final candidate vote count to the text file.
+    print("Candidate Results:")
+    txt_file.write("Candidate Results:")
     for candidate_name in candidate_votes:
 
         # Retrieve vote count and percentage
